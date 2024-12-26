276e124b84a3d62c39de13af5170e839f0253302
Md Moniruzzaman Lelin <lelin171046@gmail.com>
2024-12-26 18:09:37 +0600

    add search section

--- src/Pages/AllJobs.jsx
+++ src/Pages/AllJobs.jsx
@@ -1,64 +1,75 @@
-import { useEffect, useState } from "react";
-import JobCard from "../Component/JobCard"
-import useAxiosSecure from "../Hook/useAxiosSecure";
-import { useQuery } from "@tanstack/react-query";
+import { useEffect, useState } from 'react'
 
-
+import axios from 'axios'
+import JobCard from '../Component/JobCard'
 
 const AllJobs = () => {
+  const [itemsPerPage, setItemsPerPage] = useState(4)
+  const [currentPage, setCurrentPage] = useState(1)
+  const [count, setCount] = useState(0)
+  const [filter, setFilter] = useState('')
+  const [sort, setSort] = useState('')
+  const [search, setSearch] = useState('')
+  const [searchText, setSearchText] = useState('')
+  const [jobs, setJobs] = useState([])
+  useEffect(() => {
+    const getData = async () => {
+      const { data } = await axios(
+        `${
+          import.meta.env.VITE_API_URL
+        }/all-jobs?page=${currentPage}&size=${itemsPerPage}&filter=${filter}&sort=${sort}&search=${search}`
+      )
+      setJobs(data)
+    }
+    getData()
+  }, [currentPage, filter, itemsPerPage, search, sort])
+  useEffect(() => {
+    const getCount = async () => {
+      const { data } = await axios(
+        `${
+          import.meta.env.VITE_API_URL
+        }/jobs-count?filter=${filter}&search=${search}`
+      )
 
+      setCount(data.count)
+    }
+    getCount()
+  }, [filter, search])
 
-    
-// const [jobs , setJobs ] = useState([]);
-const [count, setCount] = useState(0)
-const axiosSecure = useAxiosSecure()
-const [itemsPerPage, setItemsPerPage] = useState(10)
-const [currentPage, setCurrentPage] = useState(1)
+  console.log(count)
+  const numberOfPages = Math.ceil(count / itemsPerPage)
+  const pages = [...Array(numberOfPages).keys()].map(element => element + 1)
 
-
-const {data: jobs = [],
-  isLoading,
-  isError, refetch
-} = useQuery({
-  queryFn: () => getData(),
-  queryKey: ['jobs']
-})
-
-
-const getData = async () => {
-  const { data } = await axiosSecure(`/all-jobs?page=${currentPage}&size=${itemsPerPage}`)
-  return data
-}
-
-useEffect(()=>{
-  const getCount = async () =>{
-    const {data} = await axiosSecure(`/jobs-count`)
-    console.log(data.count);
-    setCount(data.count)
+  //  handle pagination button
+  const handlePaginationButton = value => {
+    console.log(value)
+    setCurrentPage(value)
   }
-  getCount()
-},[])
+  const handleReset = () => {
+    setFilter('')
+    setSort('')
+    setSearch('')
+    setSearchText('')
+  }
 
+  const handleSearch = e => {
+    e.preventDefault()
 
-// console.log(count)
-const numberOfPages = Math.ceil(count / itemsPerPage)
-const pages = [...Array(numberOfPages).keys()].map(element => element + 1)
+    setSearch(searchText)
+  }
 
-// const pages = [1,2,3,4]
-
-//Handle pagination
-
-const handlePagination = (value) =>{
- console.log(value);
-setCurrentPage(value)
-
-}
+  console.log(search)
   return (
     <div className='container px-6 py-10 mx-auto min-h-[calc(100vh-306px)] flex flex-col justify-between'>
       <div>
         <div className='flex flex-col md:flex-row justify-center items-center gap-5 '>
           <div>
             <select
+              onChange={e => {
+                setFilter(e.target.value)
+                setCurrentPage(1)
+              }}
+              value={filter}
               name='category'
               id='category'
               className='border p-4 rounded-lg'
@@ -70,11 +81,13 @@ setCurrentPage(value)
             </select>
           </div>
 
-          <form>
+          <form onSubmit={handleSearch}>
             <div className='flex p-1 overflow-hidden border rounded-lg    focus-within:ring focus-within:ring-opacity-40 focus-within:border-blue-400 focus-within:ring-blue-300'>
               <input
                 className='px-6 py-2 text-gray-700 placeholder-gray-500 bg-white outline-none focus:placeholder-transparent'
                 type='text'
+                onChange={e => setSearchText(e.target.value)}
+                value={searchText}
                 name='search'
                 placeholder='Enter Job Title'
                 aria-label='Enter Job Title'
@@ -87,8 +100,13 @@ setCurrentPage(value)
           </form>
           <div>
             <select
-              name='category'
-              id='category'
+              onChange={e => {
+                setSort(e.target.value)
+                setCurrentPage(1)
+              }}
+              value={sort}
+              name='sort'
+              id='sort'
               className='border p-4 rounded-md'
             >
               <option value=''>Sort By Deadline</option>
@@ -96,7 +114,9 @@ setCurrentPage(value)
               <option value='asc'>Ascending Order</option>
             </select>
           </div>
-          <button className='btn'>Reset</button>
+          <button onClick={handleReset} className='btn'>
+            Reset
+          </button>
         </div>
         <div className='grid grid-cols-1 gap-8 mt-8 xl:mt-16 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4'>
           {jobs.map(job => (
@@ -105,9 +125,14 @@ setCurrentPage(value)
         </div>
       </div>
 
+      {/* Pagination Section */}
       <div className='flex justify-center mt-12'>
-{/* previous btn */}
-        <button className='px-4 py-2 mx-1 text-gray-700 disabled:text-gray-500 capitalize bg-gray-200 rounded-md disabled:cursor-not-allowed disabled:hover:bg-gray-200 disabled:hover:text-gray-500 hover:bg-blue-500  hover:text-white'>
+        {/* Previous Button */}
+        <button
+          disabled={currentPage === 1}
+          onClick={() => handlePaginationButton(currentPage - 1)}
+          className='px-4 py-2 mx-1 text-gray-700 disabled:text-gray-500 capitalize bg-gray-200 rounded-md disabled:cursor-not-allowed disabled:hover:bg-gray-200 disabled:hover:text-gray-500 hover:bg-blue-500  hover:text-white'
+        >
           <div className='flex items-center -mx-1'>
             <svg
               xmlns='http://www.w3.org/2000/svg'
@@ -127,18 +152,24 @@ setCurrentPage(value)
             <span className='mx-1'>previous</span>
           </div>
         </button>
-
+        {/* Numbers */}
         {pages.map(btnNum => (
           <button
-          onClick={()=>handlePagination(btnNum)}
+            onClick={() => handlePaginationButton(btnNum)}
             key={btnNum}
-            className={`hidden px-4 py-2 mx-1 transition-colors duration-300 transform  rounded-md sm:inline hover:bg-blue-500  hover:text-white`}
+            className={`hidden ${
+              currentPage === btnNum ? 'bg-blue-500 text-white' : ''
+            } px-4 py-2 mx-1 transition-colors duration-300 transform  rounded-md sm:inline hover:bg-blue-500  hover:text-white`}
           >
             {btnNum}
           </button>
         ))}
-{/* next btn  */}
-        <button className='px-4 py-2 mx-1 text-gray-700 transition-colors duration-300 transform bg-gray-200 rounded-md hover:bg-blue-500 disabled:hover:bg-gray-200 disabled:hover:text-gray-500 hover:text-white disabled:cursor-not-allowed disabled:text-gray-500'>
+        {/* Next Button */}
+        <button
+          disabled={currentPage === numberOfPages}
+          onClick={() => handlePaginationButton(currentPage + 1)}
+          className='px-4 py-2 mx-1 text-gray-700 transition-colors duration-300 transform bg-gray-200 rounded-md hover:bg-blue-500 disabled:hover:bg-gray-200 disabled:hover:text-gray-500 hover:text-white disabled:cursor-not-allowed disabled:text-gray-500'
+        >
           <div className='flex items-center -mx-1'>
             <span className='mx-1'>Next</span>
 
